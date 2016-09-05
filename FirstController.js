5c9a2e2b2dcda69fb84384fc207567e2d172ab03
bencallis1 <bencallis1@gmail.com>
2016-09-05 13:17:24 -0600

    added some notes to the controller and the service

--- FirstController.js
+++ FirstController.js
@@ -1,29 +1,17 @@
 'use strict';
 
-myApp.controller('FirstCtrl', function ($scope, ourSvc) {
+//Create a service and inject it into the controller
 
-    $scope.reallyLike = ourSvc.reallyLike;
-    $scope.kindOfLike = ourSvc.kindOfLike;
+myApp.controller('FirstCtrl', function ($scope) {
 
-    $scope.addArtist = function() {
-        var user =  buildArtist();
-        ourSvc.addNewArtist(user);
-        removeArtist();
-    };
+    // step 1 set a variable called reallyLike on $scope. This variable will be bound to the reallyLike variable in the service
+    // step 2 set a variable called kindOfLike on $scope. This variable will be bound to the kindOfLike variable in the service
 
-    function buildArtist() {
-        return {
-            name: $scope.name,
-            genre: $scope.genre,
-            score: $scope.score
-        }
-    }
 
-    function removeArtist(){
-        $scope.name = '';
-        $scope.genre = '';
-        $scope.score = '';
-    }
+    // step 3 create a function called addArtist the will call the addNewArtist function in the service. You will need to send
+    // an artists object as the argument. How you choose to do this is up to you. Once you have added the artist clear the form
+
+
 });
 
 
--- service.js
+++ service.js
@@ -1,3 +1,10 @@
+// Step 1 create a service
+// step 2 create 2 arrays one called reallLike and the other called kingOfLike
+// step 3 create a function called addNewArtist that will accept an artist object as the argument
+// step 4 you will need to add a conditional that will check if the value of the score property on the artist object in greater or less than
+// 50. If greater than 50 then push to the reallyLike array, if less than or equal to 50 then push to the kindOfLike array
+
+// step 5 once everything is setup make sure you are binding the values of the arrays to $scope in your controller
 
 myApp.service('ourSvc', function() {
 
